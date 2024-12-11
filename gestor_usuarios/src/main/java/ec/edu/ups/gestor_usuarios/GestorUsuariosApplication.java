af8f8d271172f380842ed744b871dbd89b16277e
Darius <dfalconi@praxmed.com.ec>
2024-12-11 14:44:51 -0500

    Creación del proyecto base

---
+++ gestor_usuarios/src/main/java/ec/edu/ups/gestor_usuarios/GestorUsuariosApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.gestor_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorUsuariosApplication.class, args);
+	}
+
+}
---
+++ gestor_usuarios/src/test/java/ec/edu/ups/gestor_usuarios/GestorUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.gestor_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
